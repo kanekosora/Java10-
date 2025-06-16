f772cf44e333ec8adbdd080a9961b8a067b3c8fe
MSI\blank <zeal24360066@fsgcl.jp>
2025-06-16 11:13:55 +0900

    10章練習問題まで

--- src/GameWorld.java
+++ src/GameWorld.java
@@ -1,6 +1,8 @@
-
+import java.util.Vector;
 
 public class GameWorld {
+	static Player player;
+	static Vector<PlayerBullet> playerBullets; 
 
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
--- src/PlayerBullet.java
+++ src/PlayerBullet.java
@@ -1,7 +1,14 @@
 
 
-public class PlayerBullet {
-
+public class PlayerBullet extends Character{
+	public void draw(MyFrame f) {
+		f.setColor(200,0,0);
+		f.fillRect(x+10,y,10,10);
+	}
+	public PlayerBullet(double x,double y,double vx,double vy) {
+		//Characterクラスのコンストラクタ(8章)
+		super(x,y,vx,vy);
+	}
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
 
